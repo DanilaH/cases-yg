d6ecf5249e84da9ee836b5f03be03c2feabff712
DanilaH <41333602+DanilaH@users.noreply.github.com>
2026-09-02 23:53:45 +0500

    Type localization dictionaries structurally

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,7 +2,11 @@ import type { AppLanguage } from '../platform/yandex';
 import { en } from './en';
 import { ru } from './ru';
 
-export type Messages = typeof en;
+type DeepStrings<T> = {
+  readonly [Key in keyof T]: T[Key] extends string ? string : DeepStrings<T[Key]>;
+};
+
+export type Messages = DeepStrings<typeof en>;
 
 const messages: Readonly<Record<AppLanguage, Messages>> = { en, ru };
 
