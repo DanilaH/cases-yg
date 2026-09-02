8ef161bbeb514d1c94256487ea5baff70c91820f
DanilaH <41333602+DanilaH@users.noreply.github.com>
2026-09-03 00:33:00 +0500

    Preload reviewed slice art before opening

--- src/game/scenes/BootScene.ts
+++ src/game/scenes/BootScene.ts
@@ -1,8 +1,18 @@
 import Phaser from 'phaser';
 
+import { getRuntimeCollectibleArt, POUCH_ASSET_PATH, POUCH_TEXTURE_KEY } from '../data/artAssets';
+import { SLICE_REGISTRY } from '../data/collectibles';
+
 export class BootScene extends Phaser.Scene {
   public constructor() {
     super('BootScene');
+  }
+
+  public preload(): void {
+    this.load.image(POUCH_TEXTURE_KEY, POUCH_ASSET_PATH);
+    for (const art of getRuntimeCollectibleArt(SLICE_REGISTRY)) {
+      this.load.image(art.textureKey, art.assetPath);
+    }
   }
 
   public create(): void {
