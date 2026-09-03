eed933d81fe1595fd5a96baac12f62d2f0eac703
DanilaH <41333602+DanilaH@users.noreply.github.com>
2026-09-03 10:28:05 +0500

    Preload reviewed SFX before game ready

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import Phaser from 'phaser';
 
 import { setPlatformRuntime } from './app/runtime';
 import { createDebugPanel } from './debug/createDebugPanel';
+import { getRuntimeSfxAssets } from './game/data/audioAssets';
 import { CollectionScene } from './game/scenes/CollectionScene';
 import { BootScene } from './game/scenes/BootScene';
 import { OpeningScene } from './game/scenes/OpeningScene';
@@ -16,6 +17,7 @@ const boot = async (): Promise<void> => {
   const messages = getMessages(platform.language);
   const removeDebugPanel = createDebugPanel(platform);
   const audio = getGameAudio();
+  await audio.preloadSamples(getRuntimeSfxAssets());
 
   document.documentElement.lang = platform.language;
   document.title = messages.appTitle;
